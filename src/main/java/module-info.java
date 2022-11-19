6ebb9de5aebad51543a800c8ad98ccdd8e5b0087
Kevin-QQ-82 <kquintun@espol.edu.ec>
2022-11-18 22:37:15 -0500

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module espol.grupo_05 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens espol.grupo_05 to javafx.fxml;
+    exports espol.grupo_05;
+        opens modelo to javafx.base;
+}
